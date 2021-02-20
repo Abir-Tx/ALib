c8d84e10b9063472d718e1d72e7d23bf28da2e71
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-02-21 02:18:36 +0600

    Format all the codes using clang format
    Following the guidelines described in clang format
    This rule will be used from now on

--- include/alib/alib.hpp
+++ include/alib/alib.hpp
@@ -1,7 +1,8 @@
-/* 
-alib is a header only library which offers many usefull and frequently used C++ functions which are not present in standard c++ library.
-The library is under GPL3 license and is open for others to contribute to this project. This library supports multi platform. It works on both
-windows and Linux operating system. 
+/*
+alib is a header only library which offers many usefull and frequently used C++
+functions which are not present in standard c++ library. The library is under
+GPL3 license and is open for others to contribute to this project. This library
+supports multi platform. It works on both windows and Linux operating system.
 
 The project is inspired from the rang project.
 
@@ -28,212 +29,166 @@ Maintainer:         Mushfiqur Rahman Abir
 
 // Including platform specific files
 #if defined(LINUX) || defined(MAC)
-#include <unistd.h>
 #include <sys/ioctl.h> /* For getting terminal height and width. See consoleWidth() & consoleHeight() */
+#include <unistd.h>
 #elif defined(WINDOWS)
 #include <windows.h>
 #endif
 
 // Including platform independent files
-#include "rang.hpp" /* A third party library for console text styling and coloring */
+#include <iomanip>
 #include <iostream>
 #include <vector>
-#include <iomanip>
+
+#include "rang.hpp" /* A third party library for console text styling and coloring */
 
 // Functions
-namespace alib
+namespace alib {
+// Function for dynamically decorating a text
+void decorateMe(std::string textToDecor) {
+  using namespace rang;
+
+  int charSize = textToDecor.capacity();
+  std::cout << std::endl;
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << "_"; }
+  std::cout << fg::reset;
+  std::cout << std::endl;
+
+  std::cout << fg::blue << style::bold << textToDecor << style::reset
+            << fg::reset;
+
+  std::cout << std::endl;
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << "_"; }
+  std::cout << fg::reset;
+  std::cout << std::endl;
+  std::cout << std::endl;
+}
+
+void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber) {
+  using namespace rang;
+
+  int charSize = textToDecor.capacity();
+  std::cout << std::endl;
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << "_"; }
+  std::cout << fg::reset;
+  for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+  std::cout << fg::blue << style::bold << textToDecor << style::reset
+            << fg::reset;
+
+  for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << "_"; }
+  std::cout << fg::reset;
+  std::cout << std::endl;
+  std::cout << std::endl;
+}
+
+void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber,
+                std::string decorator) {
+  using namespace rang;
+
+  int charSize = textToDecor.capacity();
+  std::cout << std::endl;
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << decorator; }
+  std::cout << fg::reset;
+  for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+  std::cout << fg::blue << style::bold << textToDecor << style::reset
+            << fg::reset;
+
+  for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+  std::cout << fg::red;
+  for (int i = 0; i < (charSize * 2); i++) { std::cout << decorator; }
+  std::cout << fg::reset;
+  std::cout << std::endl;
+  std::cout << std::endl;
+}
+
+/* Clears the screen depending on OS */
+void clrscr() {
+#if defined(LINUX) || defined(MAC)
+  system("clear");
+#elif defined(WINDOWS)
+  system("cls");
+#endif
+}
+
+// Return the console's width and height respectively
+unsigned int consoleWidth() {
+#if defined(LINUX) || defined(MAC)
+  winsize conSize;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &conSize);
+  return conSize.ws_col;
+#elif defined(WINDOWS)
+  int width;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  width = (int)(csbi.dwSize.X);
+  return width;
+#endif
+}
+
+unsigned int consoleHeight()
+
 {
-    // Function for dynamically decorating a text
-    void decorateMe(std::string textToDecor)
-    {
-        using namespace rang;
-
-        int charSize = textToDecor.capacity();
-        std::cout << std::endl;
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << "_";
-        }
-        std::cout << fg::reset;
-        std::cout << std::endl;
-
-        std::cout << fg::blue << style::bold << textToDecor << style::reset << fg::reset;
-
-        std::cout << std::endl;
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << "_";
-        }
-        std::cout << fg::reset;
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber)
-    {
-        using namespace rang;
-
-        int charSize = textToDecor.capacity();
-        std::cout << std::endl;
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << "_";
-        }
-        std::cout << fg::reset;
-        for (int i = 1; i <= lineBreaksNumber; i++)
-        {
-            std::cout << std::endl;
-        }
-        std::cout << fg::blue << style::bold << textToDecor << style::reset << fg::reset;
-
-        for (int i = 1; i <= lineBreaksNumber; i++)
-        {
-            std::cout << std::endl;
-        }
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << "_";
-        }
-        std::cout << fg::reset;
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber, std::string decorator)
-    {
-        using namespace rang;
-
-        int charSize = textToDecor.capacity();
-        std::cout << std::endl;
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << decorator;
-        }
-        std::cout << fg::reset;
-        for (int i = 1; i <= lineBreaksNumber; i++)
-        {
-            std::cout << std::endl;
-        }
-        std::cout << fg::blue << style::bold << textToDecor << style::reset << fg::reset;
-
-        for (int i = 1; i <= lineBreaksNumber; i++)
-        {
-            std::cout << std::endl;
-        }
-        std::cout << fg::red;
-        for (int i = 0; i < (charSize * 2); i++)
-        {
-            std::cout << decorator;
-        }
-        std::cout << fg::reset;
-        std::cout << std::endl;
-        std::cout << std::endl;
-    }
-
-    /* Clears the screen depending on OS */
-    void clrscr()
-    {
 #if defined(LINUX) || defined(MAC)
-        system("clear");
+  winsize conSize;
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &conSize);
+  return conSize.ws_row;
 #elif defined(WINDOWS)
-        system("cls");
+  int height;
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+  GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+  height = (int)(csbi.dwSize.Y);
+  return height;
 #endif
-    }
-
-    // Return the console's width and height respectively
-    unsigned int consoleWidth()
-    {
-#if defined(LINUX) || defined(MAC)
-        winsize conSize;
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &conSize);
-        return conSize.ws_col;
-#elif defined(WINDOWS)
-        int width;
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-        width = (int)(csbi.dwSize.X);
-        return width;
-#endif
-    }
-
-    unsigned int consoleHeight()
-
-    {
-#if defined(LINUX) || defined(MAC)
-        winsize conSize;
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &conSize);
-        return conSize.ws_row;
-#elif defined(WINDOWS)
-        int height;
-        CONSOLE_SCREEN_BUFFER_INFO csbi;
-        GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-        height = (int)(csbi.dwSize.Y);
-        return height;
-#endif
-    }
-    // Alias for the consoleWidth & consoleHeight funcs
+}
+// Alias for the consoleWidth & consoleHeight funcs
 #define terminalWidth consoleWidth
 #define terminalHeight consoleHeight
 
-    // Another overload of decorateMe function with print to center ability
-    void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber, std::string decorator, bool isCenter)
-    {
-        if (isCenter)
-        {
-            using namespace rang;
+// Another overload of decorateMe function with print to center ability
+void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber,
+                std::string decorator, bool isCenter) {
+  if (isCenter) {
+    using namespace rang;
 
-            int charSize = textToDecor.capacity();
+    int charSize = textToDecor.capacity();
 
-            // Getting the terminal center value & starting point of the decorations
-            unsigned int termCenter = consoleWidth() / 2;
-            int startingPoint = termCenter - (charSize / 2);
-            int textToDecor_StartingPoint = startingPoint + ((charSize * 2) - charSize);
+    // Getting the terminal center value & starting point of the decorations
+    unsigned int termCenter = consoleWidth() / 2;
+    int startingPoint = termCenter - (charSize / 2);
+    int textToDecor_StartingPoint = startingPoint + ((charSize * 2) - charSize);
 
-            std::cout << std::endl;
-            std::cout << fg::red;
-            std::cout << std::setw(startingPoint) << std::setfill(' ');
-            for (int i = 0; i < (charSize * 2); i++)
-            {
-                std::cout << decorator;
-            }
-            std::cout << fg::reset;
-            for (int i = 1; i <= lineBreaksNumber; i++)
-            {
-                std::cout << std::endl;
-            }
-            std::cout << fg::blue << style::bold << std::setw(textToDecor_StartingPoint) << std::setfill(' ') << textToDecor << style::reset << fg::reset;
+    std::cout << std::endl;
+    std::cout << fg::red;
+    std::cout << std::setw(startingPoint) << std::setfill(' ');
+    for (int i = 0; i < (charSize * 2); i++) { std::cout << decorator; }
+    std::cout << fg::reset;
+    for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+    std::cout << fg::blue << style::bold << std::setw(textToDecor_StartingPoint)
+              << std::setfill(' ') << textToDecor << style::reset << fg::reset;
 
-            for (int i = 1; i <= lineBreaksNumber; i++)
-            {
-                std::cout << std::endl;
-            }
-            std::cout << fg::red;
-            std::cout << std::setw(startingPoint) << std::setfill(' ');
-            for (int i = 0; i < (charSize * 2); i++)
-            {
-                std::cout << decorator;
-            }
-            std::cout << fg::reset;
-            std::cout << std::endl;
-            std::cout << std::endl;
-        }
-        else
-        {
-            decorateMe(textToDecor, lineBreaksNumber, decorator);
-        }
-    }
+    for (int i = 1; i <= lineBreaksNumber; i++) { std::cout << std::endl; }
+    std::cout << fg::red;
+    std::cout << std::setw(startingPoint) << std::setfill(' ');
+    for (int i = 0; i < (charSize * 2); i++) { std::cout << decorator; }
+    std::cout << fg::reset;
+    std::cout << std::endl;
+    std::cout << std::endl;
+  } else {
+    decorateMe(textToDecor, lineBreaksNumber, decorator);
+  }
+}
 
-
-} // end of namespace alib
+}  // end of namespace alib
 
 // Undefining different platforms
 #undef LINUX
 #undef WINDOWS
 #undef MAC
 
-#endif // ALIB_DOT_HPP
+#endif  // ALIB_DOT_HPP
--- test/alibTest.cpp
+++ test/alibTest.cpp
@@ -1,23 +1,22 @@
 /* Test cpp file for alib.hpp */
 
-#include "rang.hpp"
 #include "alib.hpp"
+#include "rang.hpp"
 
 using namespace alib;
-int main()
-{
-    std::cout<<"Alib";
-    clrscr();
-    decorateMe("ALib");
-    decorateMe("ALib", 2);
-    decorateMe("ALib", 3, "+");
-    decorateMe("ALib", 3, "+", true);
-    decorateMe("ALib", 3, "+", false);
-	
-	std::cout<<(consoleWidth());
-    std::cout<<std::endl;
-	std::cout<<(terminalWidth());
-    std::cout<<std::endl;
-    
-	std::cout<<(consoleHeight());
+int main() {
+  std::cout << "Alib";
+  clrscr();
+  decorateMe("ALib");
+  decorateMe("ALib", 2);
+  decorateMe("ALib", 3, "+");
+  decorateMe("ALib", 3, "+", true);
+  decorateMe("ALib", 3, "+", false);
+
+  std::cout << (consoleWidth());
+  std::cout << std::endl;
+  std::cout << (terminalWidth());
+  std::cout << std::endl;
+
+  std::cout << (consoleHeight());
 }
