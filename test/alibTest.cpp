eef1880073309b1ca57035aa1b38b77c4d87fd80
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-06-10 00:24:52 +0600

    Add comments for clarification for future

--- test/alibTest.cpp
+++ test/alibTest.cpp
@@ -21,6 +21,21 @@ using std::endl;
 
 // Main driver function for running the tests
 int main() {
+  /* 
+  This portion is for Windows only meaning all these function wll only be tested on windows machines only
+  .Because on GitHub Windows runners some fucntions i.e  void decorateMe(std::string textToDecor, unsigned short int lineBreaksNumber,
+  std::string decorator, bool isCenter) which uses/depends on the consoleWidth & consoleHeight function
+  causes to execute the program infinitely which in result runs for over than 6 hours and 6 hours is the max limit for Windows
+  GitHub runners. So as a resul the CI breaks with error which was a false error for the developers. So to eradicate this issue
+  I have disables those functions which caused this infinite running behaviour for Windows only but kept all the tests for Linux
+  cause Linux Runners doesn't have this problem on GitHub runner as of now.
+
+  I have figured out that the console height and width of Windows Runners are very very big in github & maybe for that reason
+  this strange problem is occuring
+
+
+  TESTS FOR ALL (INCLUDING WINDOWS) ->
+  */
   clrscr();
   std::cout << "Alib\n";
   decorateMe("ALib");
@@ -37,8 +52,10 @@ int main() {
   showLoadingScreen("Loading", 4, true);
 
   std::cout << std::endl;
+
   horizontalLine(1,"red");
 
+// LINUX & MAC ONLY TESTS
 #if defined LINUX || MAC
   std::cout << "Console Width is (using consoleWidth Funtion): " <<(consoleWidth());
   std::cout << std::endl;
@@ -48,6 +65,7 @@ int main() {
   decorateMe("ALib", 3, "+", false);
   std::cout<<std::endl;
   horizontalLine(2, "purple");
+  std::cout<<std::endl;
   horizontalLine(2,"red");
   #endif
 }
