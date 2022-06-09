84c43554e4702260314aa5870c7ce32cc8e1a689
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-06-10 00:05:36 +0600

    Fix Windows and Linux testes for the GitHub Runner

--- test/alibTest.cpp
+++ test/alibTest.cpp
@@ -20,36 +20,27 @@ using std::endl;
 int main() {
   std::cout << "Alib\n";
   clrscr();
-  showLoadingScreen("Starting");
-  std::cout << std::endl;
-
-  showLoadingScreen("Loading", 4, true);
-  std::cout << std::endl;
-
   decorateMe("ALib");
   decorateMe("ALib", 2);
   decorateMe("ALib", 3, "+");
 
+  std::cout << std::endl;
+
   countdown(3, 1, true, false);
+  showLoadingScreen("Starting");
+
+  std::cout << std::endl;
+
+  showLoadingScreen("Loading", 4, true);
+
+  std::cout << std::endl;
 
 #if defined LINUX || MAC
-  decorateMe("ALib");
-  decorateMe("ALib", 2);
-  decorateMe("ALib", 3, "+");
+  std::cout << (consoleWidth());
+  std::cout << std::endl;
+  std::cout << (terminalWidth());
   decorateMe("ALib", 3, "+", true);
   decorateMe("ALib", 3, "+", false);
-  // std::cout << (consoleWidth());
-  std::cout << std::endl;
-  // std::cout << (terminalWidth());
-  std::cout << std::endl;
-
-  countdown(3, 1, true, false);
-  showLoadingScreen("Starting");
-  std::cout << std::endl;
-
-  showLoadingScreen("Loading", 4, true);
-  std::cout << std::endl;
-
-  // horizontalLine(2, "purple");
+  horizontalLine(2, "purple");
   #endif
 }
