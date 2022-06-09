a04b32c0d95b073a82520b5cebe030acfb506559
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2022-06-10 00:12:58 +0600

    Add more tests for both linux 🐧 and Windows

--- test/alibTest.cpp
+++ test/alibTest.cpp
@@ -3,6 +3,7 @@
 #include "rang.hpp"
 #include "alib.hpp"
 
+// Platform Detection Shortcuts
 #if defined(WIN32) || defined(_WIN32) || defined(_WIN64)
 #define WINDOWS
 #elif defined(__unix__) || defined(__unix) || defined(__linux__)
@@ -13,13 +14,15 @@
 #error Unknown
 #endif
 
+// Namespace usage
 using namespace alib;
-
 using std::cout;
 using std::endl;
+
+// Main driver function for running the tests
 int main() {
-  std::cout << "Alib\n";
   clrscr();
+  std::cout << "Alib\n";
   decorateMe("ALib");
   decorateMe("ALib", 2);
   decorateMe("ALib", 3, "+");
@@ -34,13 +37,17 @@ int main() {
   showLoadingScreen("Loading", 4, true);
 
   std::cout << std::endl;
+  horizontalLine(1,"red");
 
 #if defined LINUX || MAC
-  std::cout << (consoleWidth());
+  std::cout << "Console Width is (using consoleWidth Funtion): " <<(consoleWidth());
   std::cout << std::endl;
-  std::cout << (terminalWidth());
+  std::cout << "Console Width is (Using terminalWidth): "<<(terminalWidth());
+  std::cout<<std::endl;
   decorateMe("ALib", 3, "+", true);
   decorateMe("ALib", 3, "+", false);
+  std::cout<<std::endl;
   horizontalLine(2, "purple");
+  horizontalLine(2,"red");
   #endif
 }
